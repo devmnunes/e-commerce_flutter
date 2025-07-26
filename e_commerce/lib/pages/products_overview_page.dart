6a6773d2f6e8b001bb92d4d8114a06d4a44e2fba
Matheus Nunes <matheus_gustavo97@hotmail.com>
2025-07-26 14:29:48 -0300

    commit

--- e_commerce/lib/pages/products_overview_page.dart
+++ e_commerce/lib/pages/products_overview_page.dart
@@ -1,9 +1,7 @@
-import 'package:e_commerce/components/product_item.dart';
-import 'package:e_commerce/data/dummy_data.dart';
-import 'package:e_commerce/models/product.dart';
-import 'package:e_commerce/models/product_list.dart';
+
+import 'package:e_commerce/components/product_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 
 class ProductsOverviewPage extends StatelessWidget {
   
@@ -12,24 +10,15 @@ class ProductsOverviewPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadProducts = provider.items;
     
     return Scaffold(
       appBar: AppBar(title: Text('Minha Loja')),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-          ),
-          itemCount: loadProducts.length,
-          itemBuilder: (ctx, i) => ProductItem(product: loadProducts[i]),
-        ),
+        child: ProductGrid(),
       ),
     );
   }
 }
+
+
