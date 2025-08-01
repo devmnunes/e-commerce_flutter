8573225d50c27d170f15adff2eb715385ae9ba4d
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-08-01 11:47:24 -0300

    commit

--- e_commerce/lib/components/product_item.dart
+++ e_commerce/lib/components/product_item.dart
@@ -7,6 +7,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context);
+    
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
--- e_commerce/lib/models/product_list.dart
+++ e_commerce/lib/models/product_list.dart
@@ -4,9 +4,23 @@ import 'package:flutter/material.dart';
 
 class ProductList with ChangeNotifier {
    List<Product> _items = dummyProducts;
+   bool _showFavoriteOnly = false;
 
    List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
      return [..._items];
+   }
+
+   void showFavoriteOnly () {
+    _showFavoriteOnly = true;
+    notifyListeners();
+   }
+
+   void showAll () {
+    _showFavoriteOnly = false;
+    notifyListeners();
    }
 
    void addProduct(Product product) {
