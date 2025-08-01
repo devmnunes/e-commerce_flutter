d9bbd0b1dee94420882a406f06dbecb238c364a5
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-08-01 13:50:18 -0300

    commit

--- e_commerce/lib/pages/products_overview_page.dart
+++ e_commerce/lib/pages/products_overview_page.dart
@@ -1,38 +1,29 @@
-
 import 'package:e_commerce/components/product_grid.dart';
 import 'package:e_commerce/models/product.dart';
 import 'package:e_commerce/models/product_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-enum FilterOptions {
-  Favorites,
-  All,
-}
-
+enum FilterOptions { Favorites, All }
 
 class ProductsOverviewPage extends StatelessWidget {
-  
   ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<ProductList>(context);
     return Scaffold(
-      appBar: AppBar(title: Text('Minha Loja'),
-      iconTheme: 
-        IconThemeData(
-          color: Colors.white,
-          ),
-            actions: [
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
           PopupMenuButton(
             itemBuilder: (_) => [
-              PopupMenuItem(child: Text('Somente favoritos'),
-              value: FilterOptions.Favorites,
+              PopupMenuItem(
+                child: Text('Somente favoritos'),
+                value: FilterOptions.Favorites,
               ),
-              PopupMenuItem(child: Text('Todos'),
-              value: FilterOptions.All,
-              ),
+              PopupMenuItem(child: Text('Todos'), value: FilterOptions.All),
             ],
             onSelected: (FilterOptions selectValue) {
               if (selectValue == FilterOptions.Favorites) {
@@ -44,14 +35,8 @@ class ProductsOverviewPage extends StatelessWidget {
           ),
         ],
       ),
-      
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ProductGrid(),
-      ),
-      
+
+      body: Padding(padding: const EdgeInsets.all(10), child: ProductGrid()),
     );
   }
 }
-
-
